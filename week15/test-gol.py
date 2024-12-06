5bc9fba14d021606dd1ce06486c05beac857338d
Ethan Clay (UWE) <ethan2.clay@live.uwe.ac.uk>
2024-12-06 10:23:27 +0000

    Add conway's game of life

---
+++ week15/test-gol.py
@@ -0,0 +1,12 @@
+# simple programs level 1
+import unittest
+from task1 import *
+
+import unittest
+
+class GOLFunctions(unittest.TestCase):
+    def test_underpopulation(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main(argv=[''], verbosity=2, exit=False)
